cc5f07a54dc883fa650d3fcc1a8474324b5390f0
Pokoik Lucia <pokoiklucia@gmail.com>
2025-02-19 12:44:36 -0300

    cambio de musica

--- assets/musica/musica_sonidos.py
+++ assets/musica/musica_sonidos.py
@@ -1,3 +1,3 @@
-MUSICA_FONDO = "./assets/musica/musica_fondo/6.wav"
+MUSICA_FONDO = "./assets/musica/musica_fondo/music_fondo.wav"
 SONIDO_MUERTE = "./assets/musica/sonido_objetos/Bandage_pack_PU.wav"
 SONIDO_OBJETOS = "./assets/musica/sonido_objetos/mario-coin.mp3"
